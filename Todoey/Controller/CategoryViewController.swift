e8ba04d43587f6eba248d1e1d1fa520d4533cb1f
Ryan A. Anderson <ryan.anderson9921@stu.mtec.edu>
2022-01-31 20:47:02 -0700

    Refactoring for Multiple Categories

---
+++ Todoey/Controller/CategoryViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Ryan Anderson on 1/31/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+/// Add New Catrgories
+    @IBAction func addButtonPressed(_ sender: Any) {
+    }
+    
+    //MARK: - TableView DataSource Methods
+//  Displaying all the Categoryies in the presistance Container
+    
+    //MARK: - TableView Delegate Methods
+// leave blank for now
+    
+    //MARK: - Data Manipulation Methods
+//    Save data and load data
+  
+}
