9fbf4d9ec0b9121deaf8675bfa67b9575abf19e3
Ryan A. Anderson <ryan.anderson.dhhs@gmail.com>
2022-02-20 21:46:05 -0700

    Setting up dataModels

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Ryan Anderson on 2/20/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name: String = ""
+    let items = List<Item>()
+}
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ryan Anderson on 2/20/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Item: Object {
+    @objc dynamic var title: String = ""
+    @objc dynamic var done: Bool = false
+    var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
+}
